91652c77a89bfbc736a9c9c09336b10503d5c407
Ken <tranthamkw@unk.edu>
2019-04-05 23:54:33 -0500

    working code to calc diffraction

--- calcIntensity.c
+++ calcIntensity.c
@@ -1,4 +1,4 @@
-
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -6,43 +6,54 @@
 #define PI 3.141592654
 
 int main (int argc, char **argv){
-	double R, a, Eq, Iq, q, t, dy, dt, r, y;
-	unsigned int i, j;
+	double R, a, Eq, Iq, q, t, dy, dt, r, y, Eo;
+	unsigned int i, j,k;
 	unsigned int numysteps;
 	unsigned int numtsteps;
-
+	FILE *fp;
+	char filename[32];
 
 if (argc==4){
 	R = atof(argv[1]);
 	a = atof(argv[2]);
-	q = atof(argv[3]);
+	strcpy(filename,argv[3]);
+
+	fp = fopen(filename,"w");
+	if (!fp){
+		printf("unable to open file %s\n",filename);
+		return 1;
+		}
+	fprintf(fp,"R,a,q,Iq\n");
+
 	// convert q to radians
-	q = q*PI/180.0;
+	Eo = 100.0;
 
 	numysteps = 100;
 	numtsteps = 100;
 
 	dt = 1/(double)numtsteps;
-	dy = 1/(double)numysteps;
+	dy = a/(double)numysteps;
 
+	for (k=0;k<(90*4);k++){ //angle ; every 0.25 degrees
+	q =((double)k)*PI/(180.0*4.0);// convert k in degrees to radians
 	Iq=0; // intensity at some angle q
 	for (j=0;j<numtsteps;j++){
 		t = dt*(double)j;
-		Eq = 0;
+		Eq = 0;// electric field at some angle q
 		for (i=0;i<numysteps;i++){
-			y = (-1.0*a/2.0) + dy *(double)i;
-			r = sqrt(pow(R,2.0) + pow(y,2.0) - 2*y*R*sin(q));
-			Eq+=((sin(2*PI*(t - r)))*dy)/r;
-//			printf("%f %f\t",r,Eq);
+			y = (dy *(double)i)-(a/2.0);
+			r = sqrt(pow(R,2.0) + pow(y,2.0) - 2.0*y*R*sin(q));
+			Eq+=Eo*((sin(2*PI*(t - r)))*dy)/r;
 		}
 		Iq+= pow(Eq,2.0)*dt;
-		printf("%d\t%e\n",j,Iq);
 	}
 
-	printf("R=%f\ta=%f\tq=%fradians\n",R,a,q);
-
+	printf("R=%.2f\ta=%.2f\tq=%.2f degrees\t%e\n",R,a,q*180.0/PI,Iq);
+	fprintf(fp,"%.2f,%.2f,%.2f,%e\n",R,a,q*180.0/PI,Iq);
+	}
+	fclose(fp);
 } else {
-	printf ("usage: ./calcIntensity <R> <a> <theta(degrees)> \n");
+	printf ("usage: ./calcIntensity <R> <a> filename\n");
 }
 return 0;
 
